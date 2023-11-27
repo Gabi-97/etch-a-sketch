384efa1e294af99306ca26259b4d2c60327cf3eb
Feyru <cakeesky@gmail.com>
2023-11-27 13:15:28 +0100

    add rainbow color option button add reset button

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
 
 document.addEventListener('DOMContentLoaded', addElement);
 
-// function addElement() {
-//     let container = document.querySelector('.container');
-//     for(let i =0; i<16; i++) {
-//         let horizontal = document.createElement('div');
-//         horizontal.setAttribute('class', 'horizontal');
-//         container.appendChild(horizontal);
-//         for (let j =0; j < 16; j++) {
-//             let vertical = document.createElement('div');
-//             vertical.setAttribute('class', 'vertical');
-//             container.appendChild(vertical);
-//         }
-//     }
-// }
-
-function addElement() { 
+const btn = document.querySelector('button');
 let container = document.querySelector('.container');
-    for (let i=0; i<16; i++) {
-        for (let j=0; j<16; j++) { 
-        let cell = document.createElement('div');
-        cell.setAttribute('class', 'cell');
-        container.appendChild(cell);
-        }
+btn.addEventListener('click', getUserInput);
+
+//creating grid based on users input
+function getUserInput() {
+    container.innerHTML = '';
+    const userInput = prompt('How many squares would you like?');
+    if(userInput>100) {
+        console.log('too big number');
     }
+    else {
+        for(let i=0; i<userInput; i++) {
+            for(let j=0; j<userInput; j++) {
+                let cell = document.createElement('div');
+                cell.setAttribute('class', 'cell');
+                cell.style.width = 512/userInput + 'px';
+                cell.style.height = 512/userInput + 'px';
+                container.appendChild(cell);
+            }
+        } 
+        colorGrids();
+} 
+
 }
+
+//creating 16x16 grid
+function addElement() { 
+        for (let i=0; i<16; i++) {
+            for (let j=0; j<16; j++) { 
+            let cell = document.createElement('div');
+            cell.setAttribute('class', 'cell');
+            container.appendChild(cell);
+            }
+        }
+        colorGrids();
+}
+//function to change the squares background on mose over
+    function colorGrids() { 
+    let cell = document.querySelectorAll('.cell');
+    cell.forEach(cell => { 
+    cell.addEventListener("mouseenter", changeColor);
+        });
+    function changeColor(event) {
+        let currentCell = event.target;
+        currentCell.style.backgroundColor = 'black';
+    }   
+}
+
+//add warning if the input number is bigger than 100;
+//add a reset button which resets the grid size to the original size
+//randomize the color 
+
